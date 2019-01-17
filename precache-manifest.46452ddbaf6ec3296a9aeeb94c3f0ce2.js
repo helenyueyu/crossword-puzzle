3a3f12bdb8fbd0675a2269e44a923b50b9de3e8b
tokyosuite <helenyueyu@gmail.com>
2019-01-17 08:42:25 -0800

    Updates

--- precache-manifest.46452ddbaf6ec3296a9aeeb94c3f0ce2.js
+++ precache-manifest.46452ddbaf6ec3296a9aeeb94c3f0ce2.js
@@ -28,12 +28,12 @@ self.__precacheManifest = [
     "url": "/crossword-puzzle/static/media/outline-icons.ef60a4f6.woff"
   },
   {
-    "revision": "701ae6abd4719e9c2ada3535a497b341",
-    "url": "/crossword-puzzle/static/media/outline-icons.701ae6ab.eot"
-  },
-  {
     "revision": "9c74e172f87984c48ddf5c8108cabe67",
     "url": "/crossword-puzzle/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/crossword-puzzle/static/media/outline-icons.701ae6ab.eot"
   },
   {
     "revision": "ad97afd3337e8cda302d10ff5a4026b8",
@@ -56,16 +56,16 @@ self.__precacheManifest = [
     "url": "/crossword-puzzle/static/media/brand-icons.a046592b.woff"
   },
   {
-    "revision": "13db00b7a34fee4d819ab7f9838cc428",
-    "url": "/crossword-puzzle/static/media/brand-icons.13db00b7.eot"
-  },
-  {
     "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
     "url": "/crossword-puzzle/static/media/icons.8e3c7f55.eot"
   },
   {
     "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
     "url": "/crossword-puzzle/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/crossword-puzzle/static/media/brand-icons.13db00b7.eot"
   },
   {
     "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
@@ -84,7 +84,7 @@ self.__precacheManifest = [
     "url": "/crossword-puzzle/static/media/brand-icons.a1a749e8.svg"
   },
   {
-    "revision": "c8b099634ad6a95fb111a0e688b7c0cb",
+    "revision": "952acc5f508eaeb30d99bb8d76820468",
     "url": "/crossword-puzzle/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/crossword-puzzle/precache-manifest.657045990f49de7887de52ca83289afb.js"
+  "/crossword-puzzle/precache-manifest.46452ddbaf6ec3296a9aeeb94c3f0ce2.js"
 );
 
 workbox.clientsClaim();
